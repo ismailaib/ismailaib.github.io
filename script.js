72ba8385cae324897fb16365483bcb4895c5191b
ismailaib <ismailaitbouhmad@gmail.com>
2022-09-23 21:03:08 +0100

    add loading pages

--- script.js
+++ script.js
@@ -81,7 +81,36 @@ function CNT() {
     alert("pageXOffset: " + window.scrollX + ", scrollY: " + window.scrollY);
   }
 
+  var myVar;
+
+  function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+  }
+  
+  function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+  }
 
 
 
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
+anime.timeline({loop: true})
+  .add({
+    targets: '.text-2',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.letter',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
