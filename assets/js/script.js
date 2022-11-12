287884f3fc77100429cf451ebc9ceaf5c51d0d37
ismailaib <ismailaitbouhmad@gmail.com>
2022-11-12 17:52:35 +0100

    add jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,35 @@
-        // Parallax Code
-        var scene = document.getElementById('scene');
-        var parallax = new Parallax(scene);
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+};
+
+window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('active')
+}
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+        strings: ["Travel App ?", "Dashboard ?", "comercial Website ?", "Portfolio ?", "Just Say Hello !"],
+        typeSpeed: 20,
+        backSpeed: 20,
+        loop: true
+    });
+
+const sr = ScrollReveal ({
+        distance: '60px',
+        duration: 2500,
+        reset:true,
+})
+sr.reveal('.home-text',{delay:200, origin:'top'})
+sr.reveal('.home-img',{delay:200, origin:'right'})
+sr.reveal('.about-text',{delay:200, origin:'right'})
+sr.reveal('.about-text .count',{delay:200, origin:'bottom'})
+sr.reveal('.about-img img',{delay:200, origin:'left'})
+sr.reveal('.center',{delay:200, origin:'right'})
+sr.reveal('.service-content .row',{delay:200, origin:'bottom'})
+sr.reveal('.resume-content .box',{delay:200, origin:'bottom'})
+sr.reveal('.contact-form',{delay:200, origin:'bottom'})
