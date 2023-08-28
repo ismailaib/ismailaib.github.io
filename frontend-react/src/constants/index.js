ef88b8478b55db999ad46236f57eca33fe20fad3
ismailaib <ismailaitbouhmad@gmail.com>
2023-08-28 23:45:23 +0100

    add code

--- frontend-react/src/constants/index.js
+++ frontend-react/src/constants/index.js
@@ -1,3 +1,5 @@
+import { about01, about02, about03, about04 } from '../assets/icons';
+import { python, laravel, react } from '../assets/icons';
 
 export const navLinks = [
     { href: "#Home", label: "Home" },
@@ -6,3 +8,64 @@ export const navLinks = [
     { href: "#Works", label: "Works" },
     { href: "#Contact me", label: "Contact me" },
 ];
+
+export const aboutData = [
+    {
+      icon: about01,
+      title: 'Front-end Developer',
+      description: 'I am a web front-end developer with 2 years of experience',
+      skillLevel: 90,
+    },
+    {
+      icon: about02,
+      title: 'Back-end Developer',
+      description: 'I am a web back-end developer with 2 years of experience',
+      skillLevel: 60,
+    },
+    {
+      icon: about03,
+      title: 'Web Designer',
+      description: 'I am a web Web Designer developer with 2 years of experience',
+      skillLevel: 40,
+    },
+    {
+      icon: about04,
+      title: 'Website Tester',
+      description: 'I am a web Website Tester developer with 2 years of experience',
+      skillLevel: 40,
+    },
+  ];
+
+
+export const skillsData = [
+    {
+      icon: python,
+      title: 'PYTHON',
+    },
+    {
+      icon: laravel,
+      title: 'LARAVEL',
+    },
+    {
+      icon: react,
+      title: 'REACT',
+    },
+  ];
+
+export  const eventData = [
+    {
+      title: 'Event 1',
+      description: 'Description of Event 1',
+      date: '2021',
+    },
+    {
+      title: 'Event 2',
+      description: 'Description of Event 2',
+      date: '2022',
+    },
+    {
+      title: 'Event 3',
+      description: 'Description of Event 3',
+      date: '2023',
+    },
+  ];
--- frontend-react/src/sections/Skills.jsx
+++ frontend-react/src/sections/Skills.jsx
@@ -1,49 +1,36 @@
 import React from 'react';
-import { arrowRight , facebook , instagram , twitter , profile , react , node , python , laravel} from "../assets/icons";
-
+import { skillsData , eventData } from "../constants";
 
 const Skills = () => {
   return (
     <section className="py-16">
       <h1 className="text-3xl text-center font-semibold mb-8">Skills</h1>
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-20">
-        <div className="grid grid-cols-3 mr-52">
-          <div className="flex flex-col items-center">
-            <div className="w-28 h-28 rounded-full bg-[#f2e3e0] flex items-center justify-center">
-              <img src={python} alt="HTML" className="w-16" />
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 mt-20">
+        <div className="gap-10 flex flex-wrap items-start justify-center flex-1">
+          {skillsData.map((skill, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <div className="w-28 h-28 rounded-full bg-[#f2e3e0] flex items-center justify-center">
+                <img src={skill.icon} alt={skill.title} className="w-16" />
+              </div>
+              <p className="text-center text-gray-700 font-semibold mt-2">{skill.title}</p>
             </div>
-            <p className="text-center font-semibold mt-2">PYTHON</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="w-28 h-28 rounded-full bg-[#f2e3e0] flex items-center justify-center">
-              <img src={laravel} alt="CSS" className=" w-16" />
-            </div>
-            <p className="text-center font-semibold mt-2">LARAVEL</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div className="w-28 h-28 rounded-full bg-[#f2e3e0] flex items-center justify-center">
-              <img src={react} alt="JavaScript" className="w-16" />
-            </div>
-            <p className="text-center font-semibold mt-2">REACT</p>
-          </div>
+          ))}
         </div>
-
-        <div className="flex flex-col space-y-4">
-          <div className="bg-white p-4 rounded shadow-md">
-            <h2 className="text-xl font-semibold">Front-end Developer</h2>
-            <p className="text-gray-600">Company XYZ - 2020 to Present</p>
-            <p className="mt-2">Worked on building responsive and user-friendly web applications using modern front-end technologies.</p>
+        <div className="container mx-auto">
+          <div className="relative flex items-center">
+            <div className="flex flex-col gap-5 justify-center items-center w-1/2">
+              {eventData.map((event, index) => (
+                <div key={index} className="mb-4">
+                  <h3 className="font-bold pb-5 text-[#FD481D] text-2xl">{event.title}</h3>
+                  <p className="text-black pb-2 text-xl">{event.description}</p>
+                  <span className="text-gray-400">{event.date}</span>
+                </div>
+              ))}
+            </div>
           </div>
-
-          <div className="bg-white p-4 rounded shadow-md">
-            <h2 className="text-xl font-semibold">Full Stack Developer</h2>
-            <p className="text-gray-600">Tech Solutions Inc. - 2018 to 2020</p>
-            <p className="mt-2">Developed and maintained web applications from front-end to back-end, including database management.</p>
-          </div>
-
-          {/* Add more experiences here */}
         </div>
       </div>
+      
     </section>
   );
 };
